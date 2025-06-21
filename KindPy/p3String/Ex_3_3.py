c16d89f5b4f4aa303ae2fc90c4bb4e17ef12d050
@JavaFramer <vla78dan@gmail.com>
2025-06-21 09:27:04 +0300

    lec_3_3_end

---
+++ KindPy/p3String/Ex_3_3.py
@@ -0,0 +1,31 @@
+# str_1 = input()
+# print(str_1[0].upper() + str_1[1:].lower())
+# res = str_1[0].upper()
+# res1 = str_1[1:].lower()
+#
+# print(res, res1)
+
+# s = input()
+# print(s.count("-"))
+
+# s = input()
+# print(s.find("ra"))
+
+# s = input()
+# print(s.replace("---", "-").replace("--", "-"))
+
+# s = input().split("\n")
+# print(s)
+# print(s.rjust(3, "0"))
+
+# s = input()
+# q = s.split(" ")
+# print(len(q))
+
+print(input().replace(" ", ";"))
+
+
+
+
+
+
