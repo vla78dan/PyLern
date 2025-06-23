b05f2f31c65de1f32c433c1484c6eeabc196467f
@JavaFramer <vla78dan@gmail.com>
2025-06-23 10:19:25 +0300

    lec_3_5_end

---
+++ KindPy/p3String/Fstring_3_5.py
@@ -0,0 +1,29 @@
+# age = 47
+# name = "Bobb"
+# print("My name is " + name + ", i'am " + str(age) + " years old.")
+# msg = "2. My name is {0}, i'am {1} years old.".format(name, age)
+# msg2 = "3. My name is {fio}, i'am {old} years old.".format(fio=name, old=age)
+#
+# msg3 = f"4. My name is {name.upper()}, i'am {age * 2} years old."
+#
+# name = input()
+# fio = input()
+# age = input()
+# msg = f"Уважаемый {name} {fio}! Поздравляем Вас с {age}-летием!"
+# print(msg)
+
+# q, w, e = input().split()
+# print(f'Габариты: {q} x {w} x {e}')
+
+# q, w = map(int, input().split())
+# print(f"{min(q, w)} {max(q, w)}")
+
+# city = input()
+# st = input()
+# num = input()
+# num1 = input()
+# print(f'г. {city}, ул. {st}, д. {num}, кв. {num1}')
+
+d = float(input())
+r = int(input())
+print(f"Вы можете получить {int(r/ d)}$ за {r} рублей по курсу {d}")
