355e1e2472ca6cdd14c5ecec7a384e01610a8df1
@JavaFramer <vla78dan@gmail.com>
2025-06-23 12:22:32 +0300

    lec_3_6_less

---
+++ KindPy/p3String/list_3_6.py
@@ -0,0 +1,52 @@
+city = ['Moscow', 'Brest', 'Salsk']
+marks = [2, 3, 4, 3, 5, 2]
+# print(int((marks[0] + marks[1] + marks[2] + marks[3] + marks[4] + marks[5]) /6))
+# print(len(city))
+# marks[0] = 4
+# marks[1] = 'three'
+# print(marks)
+# lst2 = [1, 2.5, [-1, -2, -3], 4]
+# print(len(city)) # console - 3, подсчитывает элементы списка
+#
+# temp = [25.3, 29.6, 41.2, 12.5, 36.6, 32.1]
+
+# print(min(temp)) # 12.5
+# print(max(temp)) # 41.2
+# print(sum(temp) / len(temp)) # 29.55
+#
+# print(temp) # [25.3, 29.6, 41.2, 12.5, 36.6, 32.1]
+# temp_sort = sorted(temp) # [12.5, 25.3, 29.6, 32.1, 36.6, 41.2] сортирует список по возрастанию
+# temp_sort2 = sorted(temp, reverse=True) # [41.2, 36.6, 32.1, 29.6, 25.3, 12.5] сортирует список по убыванию
+# print(temp_sort)
+# print(temp_sort2)
+
+# s = list('python') # ['p', 'y', 't', 'h', 'o', 'n']
+# s_max = max(s) # y - имеет наибольшее числовое значение
+# s_min = min(s) # h - имеет наименьшее числовое значение
+# s_sorted = sorted(s) # ['h', 'n', 'o', 'p', 't', 'y']
+
+# s_plus = [1, 2, 3] + [4, 5] # [1, 2, 3, 4, 5]
+# s_plus1 = [1, 2, 3] + [4, 5] + [True]# [1, 2, 3, 4, 5, True]
+
+# s_duble = ['I', 'like', 'python'] # ['I', 'like', 'python'] - дублирование списка
+# s_duble1 = s_duble * 3 # - ['I', 'like', 'python', 'I', 'like', 'python', 'I', 'like', 'python']
+
+s_in = ['I', 'like', 'python', 'I', 'like', 'python', 'I', 'like', 'python']
+# s_in1 = 'like' in s_in # True; in- проверка вхождения элемента в спиисок
+# s_in2 = 'pike' in s_in # False; in- проверка вхождения элемента в спиисок
+
+s_del = del s_in[3]
+print(s_in)
+
+
+
+
+
+
+
+
+
+
+
+
+
