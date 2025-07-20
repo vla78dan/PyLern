f0d2267ba80c170f97f4223c85a1c7b5f8ba8d34
@JavaFramer <vla78dan@gmail.com>
2025-07-20 22:55:44 +0300

    2-6-Smit_strLess

---
+++ aSmit/pyLess/2_5_less.py
@@ -0,0 +1,3 @@
+num_1 = 5
+num_1 = 10
+print(num_1)
--- aSmit/pyLess/2_5_number.py
+++ aSmit/pyLess/2_5_number.py
@@ -2,7 +2,61 @@
 # num_2 = int(input())
 # print(num_1 + num_2)
 
-dohod = int(input())
-ns = int(input())
-perc = int(dohod * (ns / 100))
-print(dohod - perc)
+
+# dohod = int(input())
+# ns = int(input())
+# perc = int(dohod * (ns / 100))
+# print(dohod - perc)
+
+# num = int(input())
+# degree = int(input())
+# print(num ** degree)
+
+
+# num = float(input())
+# num_fl = int(input())
+# print(round(num, num_fl))
+
+# num_1 = int(input())
+# num_2 = int(input())
+# num_3 = int(input())
+# print(max(num_1, num_2, num_3))
+
+# num_1 = int(input())
+# num_2 = int(input())
+# num_3 = int(input())
+# print(max(num_1, num_2, num_3))
+# print(min(num_1, num_2, num_3))
+
+num_1 = int(input())
+num_2 = int(input())
+num_3 = int(input())
+print(int((num_1 + num_2 + num_3) / 3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
