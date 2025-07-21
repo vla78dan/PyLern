eda90cf68a0bd35e506cee2bcf457c286a24056a
@JavaFramer <vla78dan@gmail.com>
2025-07-21 14:19:58 +0300

    2-8_fstring_less

---
+++ aSmit/pyLess/2_8_fstring.py
@@ -0,0 +1,9 @@
+name = 'Alex'
+age = 30
+
+# print('My name is ' + name + ', Age: ' + str(age) + ' years old')
+# print(f"My name is {name}, Age: {age} years old")
+
+num1 = 10
+num2 = 20
+print(f"Сумма чисел равна {num1 + num2}")
