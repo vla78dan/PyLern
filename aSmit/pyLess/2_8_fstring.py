511f9853847bbc6bd4c6e52d2db9c9f997fffc7b
@JavaFramer <vla78dan@gmail.com>
2025-07-22 10:05:39 +0300

    2-8_func_less

--- aSmit/pyLess/2_8_fstring.py
+++ aSmit/pyLess/2_8_fstring.py
@@ -1,9 +1,23 @@
-name = 'Alex'
-age = 30
+# name = 'Alex'
+# age = 30
+#
+# # print('My name is ' + name + ', Age: ' + str(age) + ' years old')
+# # print(f"My name is {name}, Age: {age} years old")
+#
+# a = 10
+# b = 20
+# # print(f"Сумма чисел равна {num1 + num2}")
+# print(f"""Hello, {name}""")
+# print(
+#     f"The sum is (a + b)")
+# print(
+# f"The sum is {sum(a, b)}")
 
-# print('My name is ' + name + ', Age: ' + str(age) + ' years old')
-# print(f"My name is {name}, Age: {age} years old")
+# name = input()
+# year = 2024 - int(input())
+# print(f"Фильму {name} в 2024 году исполнится {year}")
 
-num1 = 10
-num2 = 20
-print(f"Сумма чисел равна {num1 + num2}")
+sum_r = int(input())
+c_d = int(input())
+c_e = int(input())
+print(f"У пользователя в наличии {sum_r} рублей, за них он может получить {int(sum_r / c_d)} долларов или {int(sum_r / c_e)}  евро")
---
+++ aSmit/pyLess/2_9_input.py
@@ -0,0 +1,8 @@
+# var1 = input()
+# var2 = input()
+# res = var1 + ' ' + var2
+# print(f"My name is : is {res}")
+# print(type(var1))
+
+age=int(input('Enter your age: '))
+print(f"My age is {age}")
