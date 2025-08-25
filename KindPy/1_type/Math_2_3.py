cb02d4ad07abd8d443148fbd79fe98bdea2ad5c1
@vla78dan <vla78dan@gmail.com>
2025-08-25 15:15:27 +0300

    run

---
+++ KindPy/1_type/Math_2_3.py
@@ -0,0 +1,19 @@
+import math
+
+
+
+# a = abs(-5.6)
+# a = min(0, 5, -6, 8, 10)
+
+# a = pow(6, 2)
+# a = round(1.5)
+a = round(78.3269, 3)
+
+
+
+
+
+
+
+
+print(a)
---
+++ pyMaster/part1/5.py
@@ -0,0 +1,8 @@
+# s = 'Hello, Bob1 vHello, Bob1 Hello, Bob1 Hello, Bob1 Hello, Bob1 Hello, Bob1 Hello, Bob1 vHello, Bob1 Hello, Bob1 Hello, Bob1 Hello, Bob1 Hello, Bob1'
+#
+# num = 10
+#
+# print(s)
+
+DB_USER = 'root'
+print(DB_USER)
