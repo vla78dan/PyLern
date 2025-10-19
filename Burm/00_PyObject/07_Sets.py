380637e3060991fe7c6888392e7c7b29c0d5a24f
@vla78dan <vla78dan@gmail.com>
2025-10-19 17:12:53 +0300

    Burm_00_07_Sets.py

---
+++ Burm/00_PyObject/07_Sets.py
@@ -0,0 +1,10 @@
+myset = set()
+print(myset)
+myset.add(1)
+print(myset)
+myset.add(2)
+print(myset)
+my_list = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3]
+print(my_list)
+set_my_list = set(my_list)
+print(set_my_list)
