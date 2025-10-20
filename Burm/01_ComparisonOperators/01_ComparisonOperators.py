c681ac27cd7eda86afdbfea634a16ab6a282e2f3
@vla78dan <vla78dan@gmail.com>
2025-10-20 08:08:54 +0300

    Burm_01_01_Operators.py

---
+++ Burm/01_ComparisonOperators/01_ComparisonOperators.py
@@ -0,0 +1,12 @@
+# print(2 == 2)
+# print('hello' == 'bye')
+# print('2' != 2)
+# print(2.0 == 2)
+# print(2 >= 2)
+# q = 1<2
+# w = 2<3
+print('h'=='h' and 2 == 2)
+print('h'!='h' or 2 == 2)
+print('h'!='h')
+print(400 > 5000)
+print(not 400 > 5000)
