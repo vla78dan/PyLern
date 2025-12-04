e634dcbecfb910ff692ae88dd2766fb95220ef07
@vla78dan <vla78dan@gmail.com>
2025-12-04 19:33:21 +0300

    KindPyTwo_02_2.1_End

---
+++ KindPyTwo/_02_var/_02_less.py
@@ -0,0 +1,55 @@
+"""
+a = 7
+b = a
+print(a)
+
+a = b = c = 1
+print(a, b, c)
+print(id(a), id(b), id(c))
+
+a, b = 2.3, 1
+print(a, b)
+print(id(a), id(b))
+
+a, b = b, a
+print(a, b)
+
+print(id(a), id(b))
+
+print(type(a), type(b))
+"""
+x = 5.8
+s = 'Hello'
+print(type(x), type(s))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
