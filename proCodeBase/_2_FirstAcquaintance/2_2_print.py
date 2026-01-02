862363b92c73db0fca213afd46b8843937f7af7f
@vla78dan <vla78dan@gmail.com>
2026-01-02 18:19:11 +0300

    proCodeBase_2_2_input

--- proCodeBase/_2_FirstAcquaintance/2_2_print.py
+++ proCodeBase/_2_FirstAcquaintance/2_2_print.py
@@ -1,13 +1,32 @@
 # print("Я изучаю Python!")
+
 # print("Программирование это интересно!")
 # print()
 # print("Python это мощный язык программирования!")
-a, b, c = map(int, input().split())
-print(a, b, c, sep="-")
 
+# a, b, c = map(int, input().split())
+# print(a, b, c, sep="-")
 
+# q, w, e = input().split()
+# print(q, w, e, sep=", ")
 
+# q, w, e = input().split()
+# print(q, w, e, sep="@", end="@")
 
+# q = input()
+# w = input()
+# e = input()
+# print(q + '!',w + '.',e, sep='', end="?")
 
+# q = int(input())
+# print(q, q, sep='\n')
+
+q = int(input())
+w = int(input())
+e = int(input())
+r = int(input())
+t = int(input())
+
+print(q, w, e, r, t, sep=',', end='.')
 
 
