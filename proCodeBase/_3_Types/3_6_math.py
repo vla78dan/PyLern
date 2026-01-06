6f4d80eec6e70b9604684c8cc2c4278180b285f4
@vla78dan <vla78dan@gmail.com>
2026-01-06 16:23:25 +0300

    proCodeBase_3_6_math_8ex

--- proCodeBase/_3_Types/3_6_math.py
+++ proCodeBase/_3_Types/3_6_math.py
@@ -16,12 +16,64 @@ print(round(math.sin(math.radians(float(input()))), 5))
 
 print(round(math.pi * math.pow((float(input())), 2), 2))
 
-
-"""
 q = int(input())
 w = int(input())
-
 print(math.sqrt(q**2 + w**2))
+
+
+
+max_T  = float(input())
+min_T = float(input())
+print(abs(max_T - min_T))
+
+q = int(input())
+w = int(input())
+print(math.gcd(q, w))
+
+q=int(input())
+
+print(math.isqrt(q))
+
+
+
+
+
+"""
+
+q = int(input())
+
+print(math.log(q), math.log10(q), math.exp(math.log(q)), sep='\n')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
