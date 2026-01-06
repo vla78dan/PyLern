40d258244b3881fa1885b7599939441873dd659c
@vla78dan <vla78dan@gmail.com>
2026-01-06 03:59:03 +0300

    proCodeBase_3_5_exersize_end

--- proCodeBase/_3_Types/3_5_boolPract.py
+++ proCodeBase/_3_Types/3_5_boolPract.py
@@ -42,52 +42,40 @@ speed = float(input())
 in_range = (7.8 <= speed <= 11.2) or (16.5 <= speed <= 25.0)
 print(in_range)
 
+hight = float(input())
+print(hight >= 160)
+
+
+a, b, c, d = map(bool, input().split())
+
+print(a == b == c == d)
+
+
+a, b, c, d = map(bool, input().split())
+print(a and b and c and d)
+
+a, b, c, d = input().split()
+
+a = a == "True"
+b = b == "True"
+c = c == "True"
+d = d == "True"
+
+print(a and b and c and d)
+
+data_volume, cpu_load, memory , server_connection = input().split()
+data_volume = float(data_volume)
+cpu_load = float(cpu_load)
+memory = float(memory)
+server_connection = bool(server_connection)
+
+print(data_volume <= 1.0 and cpu_load<= 70.0 and memory>= 16.0 and server_connection == True)
+
+
+
 
 """
 
+q, w, e = map(int, input().split())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(q > 10 and w > 10 and e > 10)
