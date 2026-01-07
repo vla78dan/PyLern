d6b84ccfde84f627b575eafb1a2128b1256b3ec4
@vla78dan <vla78dan@gmail.com>
2026-01-07 23:35:26 +0300

    proCodeBase_3_6_math_end

--- proCodeBase/_3_Types/3_6_math.py
+++ proCodeBase/_3_Types/3_6_math.py
@@ -34,47 +34,41 @@ q=int(input())
 
 print(math.isqrt(q))
 
-
-
-
-
-"""
-
 q = int(input())
 
 print(math.log(q), math.log10(q), math.exp(math.log(q)), sep='\n')
 
 
 
+angle = math.radians(float(input()))
+a_sin = round(math.sin(angle), 6)
+a_cos = round(math.cos(angle), 6)
+a_tan = round(math.tan(angle), 6)
+print(a_sin, a_cos, a_tan, sep='\n')
 
 
+import math
+
+v = float(input())
+angle = math.radians(float(input()))
+s_angle = math.sin(angle)
+g = 9.81
+
+T = (2 * v * s_angle) / g
+H = (v ** 2 * s_angle ** 2) / (2 * g)
+R = (v ** 2 * math.sin(2 * angle)) / g
+
+print(round(T, 2), round(H, 2), round(R, 2), sep='\n')
+v = float(input())
+angle = math.radians(float(input()))
+s_angle = math.sin(angle)
+g = 9.81
+T = (2 * v * s_angle) / g
+H = ((v ** 2) * (s_angle ** 2)) / 2 * g
+R = ((v ** 2) * s_angle) / g
+print(round(T, 2), round(H, 2), round(R, 2), sep='\n')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"""
 
 
