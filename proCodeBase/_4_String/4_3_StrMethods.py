66ccd6e0f8b0b3e6f1174f759b5d62b2e305be1b
@vla78dan <vla78dan@gmail.com>
2026-01-09 08:40:14 +0300

    proCodeBase_4_3_StrMethods_ex1

---
+++ proCodeBase/_4_String/4_3_StrMethods.py
@@ -0,0 +1,41 @@
+"""
+
+q = 'python'
+print(q.capitalize()) #Python
+
+w = 'hello python'
+print(w.title())# Hello Python
+
+
+text = "Hello world! World is beautiful."
+index = text.find('World')
+print(index)
+index2 = text.find('world')
+print(index2)
+
+# 13
+# 6
+
+text2 = 'I like python'
+print(text2.replace('python', 'python2'))
+
+# I like python2
+
+
+
+
+text = 'HELLO'
+print(text.lower())
+
+str1 = input()
+print(str1.replace('Python', 'coding'))
+"""
+
+
+
+
+
+
+
+
+
