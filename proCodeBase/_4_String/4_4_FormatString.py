c56654579512b0c2e39953a2bc97d90b110ad1ed
@vla78dan <vla78dan@gmail.com>
2026-01-09 14:40:47 +0300

    proCodeBase_4_4_FormatStringEnd

---
+++ proCodeBase/_4_String/4_4_FormatString.py
@@ -0,0 +1,32 @@
+"""
+name = 'Bobb'
+age = 22
+
+print('My name is {} and i am {} years old.'.format(name, age))
+
+value = 3.14159
+print("PI: {:.2f}".format(value))
+print("PI: {:.2f}".format(value))
+print("PI: {:.3f}".format(value))
+
+name = input()
+age = input()
+town = input()
+print("Привет, {}! Тебе уже {} лет, и ты живешь в {}.".format(name, age, town))
+
+num = float(input())
+q = int(input())
+print("{:.{q}f}".format(num, q=q))
+
+name = input()
+age = input()
+hobby = input()
+print("Меня зовут {name}, мне {age} лет, и я люблю {hobby}.".format(name=name, age=age, hobby=hobby))
+
+"""
+
+
+
+
+
+
