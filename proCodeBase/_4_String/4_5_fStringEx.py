ae66a68d89d690e127fb6faa66830fbb708b34e7
@vla78dan <vla78dan@gmail.com>
2026-01-10 12:43:19 +0300

    proCodeBase_4_5_fStringEx

---
+++ proCodeBase/_4_String/4_5_fStringEx.py
@@ -0,0 +1,22 @@
+"""
+name = input()
+adress = input()
+number = input()
+
+print(f'Здравствуйте, {name}! Ваш заказ №{number} будет доставлен по адресу: {adress}.')
+
+code_str = input()
+year = int(input())
+num = int(input())
+region = int(input())
+
+print(f'Трек-номер: {code_str}-{year:02d}-{num:04d}-{region:02d}')
+
+"""
+
+
+
+
+
+
+
