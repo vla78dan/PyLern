f7c8544d9e89f37fa2851539074a5e931f55865c
@phpsturm <vla78dan@gmail.com>
2026-01-14 08:24:03 +0300

    proCode_repeat_1

---
+++ proCodeBase/_0_Repeat/repeat.py
@@ -0,0 +1,51 @@
+"""
+ Задача 4. Запросите у пользователя три числа. Найдите их среднее арифметическое и выведите результат.
+Sample Input:
+1
+2
+3
+Sample Output:
+2.0
+
+q = int(input())
+w = int(input())
+print(2 * (q + w))
+
+q = int(input())
+print(q * 7)
+
+bananas, monkeys = map(int, input().split())
+print(int(bananas / (monkeys * 3)), (bananas % (monkeys * 3)))
+
+
+P, n, r = map(float, input().split())
+print(P * (1 + r / 100) ** n)
+
+minutes = int(input())
+print((minutes // 60), (minutes % 60))
+
+
+ducks, ponds = map(int, input().split())
+print((ducks // ponds), (ducks % ponds))
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
