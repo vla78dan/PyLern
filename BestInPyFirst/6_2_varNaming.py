ed1b69a0b18c0d06e889c49f3dc9780e8634018e
@vla78dan <vla78dan@gmail.com>
2026-01-26 18:37:44 +0300

    bestInPyFirst_12_1_

--- BestInPyFirst/6_2_varNaming.py
+++ BestInPyFirst/6_2_varNaming.py
@@ -1,3 +1,5 @@
+
+"""
 # q, w, e, r, t = 'пицца', 'чизбургер', 'чизкейк', 'роллы', 'борщ'
 # print(w, t, sep='\n')
 #
@@ -11,6 +13,28 @@
 name_1 = input()
 day = input()
 print(name_1, day)
+
+car_brand = input()
+print('Ваша любимая марка:', car_brand)
+
+num1 = 50
+num2 = 25
+print(num1 - num2)
+
+num1 = 50
+num2 = 25
+print((num1 + num2), (num1 - num2), (num1 * num2),sep='\n')
+
+count_breaks = int(input())
+print('Вы установили количество перемен: ', count_breaks)
+
+num1 = int(input())
+num2 = int(input())
+print((num1 + num2), (num1 - num2), (num1 * num2),sep='\n')
+
+
+"""
+
 
 
 
