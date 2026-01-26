48ca0fbe81c70c22bb7110418361da0c406ab883
@vla78dan <vla78dan@gmail.com>
2026-01-26 12:48:34 +0300

    bestInPyFirst_6_2_varNamingEx

---
+++ BestInPyFirst/6_2_varNaming.py
@@ -0,0 +1,28 @@
+# q, w, e, r, t = 'пицца', 'чизбургер', 'чизкейк', 'роллы', 'борщ'
+# print(w, t, sep='\n')
+#
+# section = input()
+# print(section)
+
+# food = input()
+# drink = input()
+# print(food, drink, sep='\n')
+
+name_1 = input()
+day = input()
+print(name_1, day)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
