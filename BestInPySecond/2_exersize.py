b5736bb2cefefd071d2560201743b3aa5c8b5b1d
@vla78dan <vla78dan@gmail.com>
2026-01-28 12:12:16 +0300

    bestInPySecond_exersize

---
+++ BestInPySecond/2_exersize.py
@@ -0,0 +1,16 @@
+"""
+name = input("What is your name? ")
+print('Скоро выучит весь питон:',name)
+print('Скоро напишет первую игру:',name)
+print('Скоро заработает много денег:',name)
+
+course_time = 15
+hour_in_day = int(input())
+print('Вам потребуется дней:', course_time / hour_in_day)
+
+"""
+
+
+
+
+
