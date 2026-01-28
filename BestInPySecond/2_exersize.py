10794394c90dd4e4a627d5acff062f62b5ad3c40
@vla78dan <vla78dan@gmail.com>
2026-01-28 21:31:01 +0300

    bestInPySecond_exersize_4

--- BestInPySecond/2_exersize.py
+++ BestInPySecond/2_exersize.py
@@ -8,7 +8,39 @@ course_time = 15
 hour_in_day = int(input())
 print('Вам потребуется дней:', course_time / hour_in_day)
 
+
+kinder_price = 60
+kinder_piece = int(input())
+
+print('Цена одного киндера:', kinder_price)
+print('Сколько вы хотите купить:', kinder_piece)
+print('Общая стоимость покупки:', kinder_price * kinder_piece)
+
+kinder_price = 60
+
+print('Стоимость одного киндера:', kinder_price)
+print('Стоимость двух киндеров:', kinder_price * 2)
+print('Стоимость ста киндеров:', kinder_price * 100)
+
+
+
 """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ proCodeBase/_6_cycle/6_1-2_for.py
@@ -0,0 +1,48 @@
+"""
+num = int(input())
+for i in range(1, num + 1):
+    print(i)
+
+num = int(input())
+for i in range(1, num + 1):
+    if i % 2 == 0:
+        print(i)
+
+n = int(input())
+summ = 0
+for i in range(1, n + 1):
+    summ += i
+print(summ)
+
+
+n = int(input())
+summm = 0
+if n < 0:
+    print(0)
+else:
+    for i in range(1, n + 1):
+        summm += i
+    print(summm)
+
+"""
+
+# n_tick = int(input())
+# sym_tick = input()
+# if 'A' in sym_tick:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
