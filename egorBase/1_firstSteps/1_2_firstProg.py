7214b6521225ed206d19c3b8e94547df549552b7
@vla78dan <vla78dan@gmail.com>
2026-02-09 18:46:59 +0300

    egorBase_1_1-5

--- egorBase/1_firstSteps/1_2_firstProg.py
+++ egorBase/1_firstSteps/1_2_firstProg.py
@@ -1,4 +1,87 @@
+
+
+
+"""
 print(8%41)
 print(2**5)
 print(100 / 2 ** 2 )
 print(16**0.5)
+
+
+
+a = 2
+b = 7
+
+a,b = b,a
+print(a,b)
+
+print(age = 17)
+
+val = int(input())
+print(val)
+
+print(int(input()) + 1)
+
+num = int(input())
+print(num * 2, num / 2, sep='\n')
+
+
+num1 = float(input())
+print(num1 * num1)
+
+q = (int(input()))
+w = (int(input()))
+
+print(q * w)
+
+q = (int(input()))
+w = (int(input()))
+
+print(q * w, (q + w) * 2, sep='\n')
+
+
+a = float(input())
+b = float(input())
+print(abs(a - b))
+
+q = float(input())
+print(round(q, 2), round(q, 3))
+
+
+
+h1 = int(input())
+m1 = int(input())
+s1 = int(input())
+
+h2 = int(input())
+m2 = int(input())
+s2 = int(input())
+
+print(abs(((h1 * 3600) + (m1 * 60) + s1) - ((h2 * 3600) + (m2 * 60) + s2)))
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
