a3f704a2bcd43cd45daa8528695adf5d2f8a44ad
@vla78dan <vla78dan@gmail.com>
2026-02-18 10:28:32 +0300

    egorovBase_2_STR

---
+++ egorBase/2_string/2_1_string.py
@@ -0,0 +1,71 @@
+"""
+q = 'Я стану крутым программистом!'
+print(q, q, q, sep='\n')
+
+str1 = input()
+num1 = int(input())
+print(str1 * num1)
+
+print('Лев Николаевич Толстой написал "Война и мир"')
+
+q = input()
+w = input()
+print(q)
+print(w)
+
+q = input()
+w = input()
+e = input()
+print(e)
+print(w)
+print(q)
+
+str1 = input()
+print((str1 + ' ')* 4)
+
+str1 = input()
+print(len(str1))
+
+str1 = input()
+str2 = input()
+print(str1 + str2)
+
+print('Гараж' in not 'Гараж')
+
+str1, str2, str3 = map(str, input().split())
+q = str(ord(str1))
+w = str(ord(str2))
+e = str(ord(str3))
+print('Код символа ' + str1 + ' равен ' + q)
+print('Код символа ' + str2 + ' равен ' + w)
+print('Код символа ' + str3 + ' равен ' + e)
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
