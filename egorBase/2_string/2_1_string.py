04c0f85eefdb078cf3bcde0039415ece8b578372
@vla78dan <vla78dan@gmail.com>
2026-02-18 17:25:18 +0300

    egorovBase_2_3_strIndex_24End

--- egorBase/2_string/2_1_string.py
+++ egorBase/2_string/2_1_string.py
@@ -30,7 +30,12 @@ str1 = input()
 str2 = input()
 print(str1 + str2)
 
+
+
+
 print('Гараж' in not 'Гараж')
+
+
 
 str1, str2, str3 = map(str, input().split())
 q = str(ord(str1))
@@ -39,16 +44,68 @@ e = str(ord(str3))
 print('Код символа ' + str1 + ' равен ' + q)
 print('Код символа ' + str2 + ' равен ' + w)
 print('Код символа ' + str3 + ' равен ' + e)
+
+
+
+str1, str2, str3 = map(str, input().split())
+q = str(ord(str1))
+w = str(ord(str2))
+e = str(ord(str3))
+print('Код символа ' + str1 + ' равен ' + q)
+print('Код символа ' + str2 + ' равен ' + w)
+print('Код символа ' + str3 + ' равен ' + e)
+
+
+str1 = input()
+print(str1[0])
+
+
+str1 = input()
+print(str1[-1])
+
+
+s = 'qwerty123'
+print(s[5:2])
+
+
+str1 = input()
+print(str1[:4])
+
+
+str1 = input()
+print(str1[-5:])
+
+
+s = 'qwerty123'
+
+print(s[::-1])
+print(s[:])
+print(s[::2])
+print(s[5:2])
+
+s = 'qwerty123'
+
+# print(s[7:4:-1]) #21y
+print(s[4:7:-1]) #Пустая строка
+print(s[-2::-2])
+# print(s[5:2])
+
+str1 = input()
+print(str1[1::2])
+
+str1 = input()
+print(str1[::-1])
+
+str1 = input()
+print(str1[-1::-3])
+
+str1 = input()
+print(str1[-1] + str1[0:-1])
+
 
 """
 
-str1, str2, str3 = map(str, input().split())
-q = str(ord(str1))
-w = str(ord(str2))
-e = str(ord(str3))
-print('Код символа ' + str1 + ' равен ' + q)
-print('Код символа ' + str2 + ' равен ' + w)
-print('Код символа ' + str3 + ' равен ' + e)
+
 
 
 
