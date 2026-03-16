5f1f8e1d2ddb64d4255fdddd9b37ad2323b66a46
@vla78dan <vla78dan@gmail.com>
2026-03-17 01:55:50 +0300

    egorovBase_4_1_while_5-22

---
+++ egorBase/4_while/4_1_while.py
@@ -0,0 +1,76 @@
+"""
+
+i = 1
+while i <= 10:
+    print(i, end=' '))
+    i = i + 1
+#1 2 3 4 5 6 7 8 9 10
+
+i = 1
+while i < 6:
+    print('hello', end=' ')
+    i = i + 1
+#hello hello hello hello hello
+
+
+i = 20
+while i > 10:
+    print(i, end=' ')
+    i = i - 1
+20 19 18 17 16 15 14 13 12 11
+
+
+a = int(input())
+while a != 0:
+    print('Повторите ввод')
+    a = int(input())
+
+
+
+guess = input()
+password = 'qwerty'
+while guess != password:
+    print('Введите верный пароль')
+    guess = input()
+
+
+
+
+
+guess = input()
+password = 'qwerty'
+count = 0
+while guess != password:
+    count += 1
+    print('Введите верный пароль')
+    guess = input()
+print(f'Вы ввели пароль', count, 'раза')
+
+
+a = [1, 2, 33] * 5
+print(a)
+count = 0
+while 33 in a:
+    count += 1
+    a.remove(33)
+    print(count)
+    print(a)
+
+[1, 2, 33, 1, 2, 33, 1, 2, 33, 1, 2, 33, 1, 2, 33]
+1
+[1, 2, 1, 2, 33, 1, 2, 33, 1, 2, 33, 1, 2, 33]
+2
+[1, 2, 1, 2, 1, 2, 33, 1, 2, 33, 1, 2, 33]
+3
+[1, 2, 1, 2, 1, 2, 1, 2, 33, 1, 2, 33]
+4
+[1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 33]
+5
+[1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
+
+"""
+alpha = 3
+while alpha <= 24:
+    print(alpha)
+    alpha *= 2
+    print(alpha)
