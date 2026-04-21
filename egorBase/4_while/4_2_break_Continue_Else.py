1ac9ebe6699dc12084fa102947e1bc211323e755
@vla78dan <vla78dan@gmail.com>
2026-04-21 09:27:50 +0300

    ludmila_base_2-2

---
+++ egorBase/4_while/4_2_break_Continue_Else.py
@@ -0,0 +1,59 @@
+"""
+while True:
+    a = input("Enter a word: ")
+
+    if a == "exit":
+        break
+    if len(a) < 5:
+        continue
+
+    print(a, len(a))
+
+
+
+
+i = 1
+while i <= 15:
+    print(i)
+    i = i + 1
+else:
+    print('good')
+    print('job')
+print('end')
+"""
+
+
+
+t = 7
+while t > 1:
+    t -= 1
+    if t == 3 or t == 1:
+        continue
+    print(t)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
