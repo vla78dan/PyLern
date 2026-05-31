b26e6a08f0e3bf96007bc4c06641ed47d3207941
@vla78dan <vla78dan@gmail.com>
2026-05-31 15:18:34 +0300

    PRObase_6_3_iter

---
+++ proCodeBase/_6_cycle/6_3_For_iter-next.py
@@ -0,0 +1,68 @@
+"""
+for line in iter(input, 'stop'):
+    print(line)
+
+s = "cat"
+
+it = iter(s)
+
+print(next(it))
+print(next(it))
+print(next(it))
+c
+a
+t
+
+
+r = range(5)
+it = iter(r)
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+0
+1
+2
+3
+4
+
+
+
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
