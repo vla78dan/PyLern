e39d8b36f3ae43a3f9b049f8cfb466a894dc19ff
@vla78dan <vla78dan@gmail.com>
2026-06-01 10:53:37 +0300

    PRObase_6_3_iter_1

--- proCodeBase/_6_cycle/6_3_For_iter-next.py
+++ proCodeBase/_6_cycle/6_3_For_iter-next.py
@@ -27,14 +27,46 @@ print(next(it))
 3
 4
 
+it = iter('abcd')
+
+first = next(it)
+print(first)
+
+for ch in it:
+    print(ch)
+
+n = 5
+it = iter(range(n))
+next(it)
+second = next(it)
+
+print(second)
+
+_____________________________________________________________________________
+Шаг 1. Считайте строку s .
+
+Шаг 2. Выделите первый символ строки и оставшуюся часть.
+
+Шаг 3. Выведите две строки:
+first=<...>
+tail=<...>
+
+Шаг 4. Контракт: после выполнения должны существовать s, first, tail.
+
+Важно: проверка ожидает, что в решении используются iter() и next().
 
 
+s = input()
+it = iter(s)
+first = next(it)
+tail = ''.join(it)
 
+
+print(f'first={first}')
+print(f'tail={tail}')
 
 
 """
-
-
 
 
 
