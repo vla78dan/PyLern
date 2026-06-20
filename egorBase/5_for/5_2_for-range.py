42c50ceba74ce3eaabc7a97f73665236c01db631
@vla78dan <vla78dan@gmail.com>
2026-06-20 17:02:31 +0300

    egorBase_5_2_for-range

---
+++ egorBase/5_for/5_2_for-range.py
@@ -0,0 +1,79 @@
+# for i in range(4):
+#     print(i)
+
+# sum = 0
+# for i in range(10, 100):
+#     sum = sum + i
+# print(sum) # 4905
+
+# factirial
+
+# pr = 1
+# for i in range(1, 4):
+#     pr = pr * i
+# print(pr) #6
+
+# n = int(input())
+# pr = 1
+# for i in range(1, n + 1):
+#     pr *= i
+# print(pr)
+
+
+# for i in range(4):
+#     print('hello')
+# hello
+# hello
+# hello
+# hello
+
+from random import randint
+
+# for i in range(5):
+#     a = randint(1, 100)
+#     print(a)
+#
+# 18
+# 32
+# 11
+# 57
+# 18
+
+# s = 0
+# for i in range(3):
+#     a = randint(1, 10)
+#     s = s + a
+#     print(a, end=" ")
+# print()
+# print(s)
+# 3 4 8
+# 15
+
+# for i in range(1, 6):
+#     print('*' * i)
+# *
+# **
+# ***
+# ****
+# *****
+
+# n = int(input())
+# s = 0
+# for i in range(n):
+#     a = int(input())
+#     s = s + a
+#     print('current s:', s)
+# print('total s:', s)
+"""
+Программа принимает на вход натуральное число N. Ваша задача — вывести на экран все числа от 1 до N, каждое число на отдельной строке.
+"""
+N = int(input())
+for i in range(1, N+1):
+    print(i)
+
+
+
+
+
+
+
