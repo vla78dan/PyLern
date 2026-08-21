5db3595db9125fb922b75142cefa94a2f796dc28
@vla78dan <vla78dan@gmail.com>
2026-08-21 12:26:08 +0300

    pyForDjango_GenPyFirst_4_1_ifElse

---
+++ pyForDjango/generationFirst/2_2_print.py
@@ -0,0 +1,85 @@
+"""# print('Я люблю Питон')
+# print('I', 'Lern', 'Python', 'For', 'Django')
+#
+# print('4', '8', '15', '16', '23', '42', sep='\n')
+
+print('*', "*" * 2,"*" * 3,"*" * 4,"*" * 5,"*" * 6,"*" * 7, sep='\n')
+print('What is your name?')
+print('Hi!', input())
+
+
+
+
+var_name = input()
+print('Вы ввели текст', var_name)
+
+name = 'Dodddd'
+city = 'Bashanta'
+print('My name', name, '.', 'City:', city)
+
+print('What is your name?')
+name = input()
+print(name)
+
+name = input()
+print('Привет ,', name)
+
+
+football_team = input()
+print(football_team, ' - чемпион!')
+
+st1 = input()
+st2 = input()
+st3 = input()
+
+print(st1, st2, st3, sep='\n')
+
+st1 = input()
+st2 = input()
+st3 = input()
+
+print(st3, st2, st1, sep='\n')
+
+print('YES', sep='!', end='#')
+print('NO', sep='#', end='!')"""
+
+print('БЕСКОНЕЧНОСТЬ', 'НЕ', 'ПРЕДЕЛ', sep='_', end='')
+print('!')
+print('БЕСКОНЕЧНОСТЬ_НЕ_ПРЕДЕЛ', end='')
+print('!')
+
+print('БЕСКОНЕЧНОСТЬ_', 'НЕ_', 'ПРЕДЕЛ!', sep='', end='')
+
+print('БЕСКОНЕЧНОСТЬ_НЕ_ПРЕДЕЛ!')
+
+print('БЕСКОНЕЧНОСТЬ', 'НЕ', 'ПРЕДЕЛ', '!', sep='_')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
