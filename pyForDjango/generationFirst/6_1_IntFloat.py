a3e1610c4d4f106e8992ce7fde88630614d585d9
@vla78dan <vla78dan@gmail.com>
2026-08-23 15:57:53 +0300

    pyForDjango_GenPyFirst_6_1_intFloatMinMaxAbs

---
+++ pyForDjango/generationFirst/6_1_IntFloat.py
@@ -0,0 +1,45 @@
+"""
+num1 = float(3.0)
+num2 = float(2 - 2)
+
+print(num1 * num2)
+
+a  = float(input())
+b = float(input())
+print((0.5 * a) * b)
+
+s = float(input())
+v1 = float(input())
+v2 = float(input())
+print(s / (v1 + v2))
+
+num = float(input())
+if num == 0:
+    print('Обратного числа не существует')
+else:
+    print( 1 / num)
+
+
+Tf = float(input())
+print((5 / 9) * (Tf - 32))
+
+\
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+n4 = int(input())
+n5 = int(input())
+print('Наименьшее число =', min(n1, n2, n3, n4, n5))
+print('Наибольшее число =', max(n1, n2, n3, n4, n5))
+
+
+
+"""
+a1 = abs(float(input()))
+a2 = abs(float(input()))
+a3 = abs(float(input()))
+a4 = abs(float(input()))
+a5 = abs(float(input()))
+print(a1 + a2 + a3 + a4 + a5)
+
+
