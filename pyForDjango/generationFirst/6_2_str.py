4df9d9cb443afb8383939aa65da8c4504adffe42
@vla78dan <vla78dan@gmail.com>
2026-08-25 21:35:24 +0300

    pyForDjango_GenPyFirst_6_2_string

---
+++ pyForDjango/generationFirst/6_2_str.py
@@ -0,0 +1,26 @@
+import random
+"""
+num = random.randint(1, 45)
+print(num)
+
+num = random.randint(1, 4)
+print(num)
+
+print(f"Python is a great language!, said Fred. I don't ever remember having this much fun before.")
+
+str1 = ', said Fred.'
+print('"Python is a great language!"' + str1 + '"I don\'t ever remember having this much fun before."')
+
+name = input()
+l_name = input()
+print('Hello ' + name + ' ' + l_name +'! You have just delved into Python')
+"""
+
+
+
+
+
+
+
+
+
