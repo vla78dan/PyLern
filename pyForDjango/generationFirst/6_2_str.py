9bdee5e9acaa14506462df22983bea617bcca8c5
@vla78dan <vla78dan@gmail.com>
2026-08-30 13:33:31 +0300

    pyForDjango_GenPyFirst_6_2_string_end

--- pyForDjango/generationFirst/6_2_str.py
+++ pyForDjango/generationFirst/6_2_str.py
@@ -14,7 +14,43 @@ print('"Python is a great language!"' + str1 + '"I don\'t ever remember having t
 name = input()
 l_name = input()
 print('Hello ' + name + ' ' + l_name +'! You have just delved into Python')
+
+
+name_f = input()
+l_name_f = len(name_f)
+
+print('Футбольная команда ' +  name_f + ' имеет длину ' + str(l_name_f) + ' символов ')
+
+
+str1 = input()
+str2 = input()
+str3 = input()
+
+min_str = min(str1, str2, str3,key=len)
+max_str = max(str1, str2, str3,key=len)
+print(min_str, max_str, sep='\n')
+
+
+s1 = input()
+if 'синий' in s1 :
+    print('YES')
+else:
+    print('NO')
+    
+    
+s1 = input()
+if 'суббота' in s1 or 'воскресенье' in s1:
+    print('YES')
+else:
+    print('NO')
 """
+s1 = input()
+if '@' in s1 and '.' in s1:
+    print('YES')
+else:
+    print('NO')
+
+
 
 
 
