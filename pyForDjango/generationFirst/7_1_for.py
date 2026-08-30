075494592871ba7d81e12b9b622f45676affe1be
@vla78dan <vla78dan@gmail.com>
2026-08-30 14:55:09 +0300

    pyForDjango_GenPyFirst_7_1_for

---
+++ pyForDjango/generationFirst/7_1_for.py
@@ -0,0 +1,42 @@
+"""
+
+for i in range(3):
+    num = int(input())
+    print('Квадрат числа равен:', num*num)
+print('End for')
+
+
+print('End for')
+print('End for1')
+
+for i in range(3):
+    print('3')
+    print('4')
+print('End for2')
+
+
+for i in range(10):
+    print('Python is awesome!')
+
+
+
+for _ in range(6):
+    print('AAA')
+for _ in range(5):
+    print('BBBB')
+print('E')
+for _ in range(9):
+    print('TTTT')
+print('G')
+
+s1 = input()
+n = int(input())
+for i in range(n):
+    print(s1)
+"""
+n = int(input())
+for i in range(n):
+    print(19 * "*")
+
+
+
