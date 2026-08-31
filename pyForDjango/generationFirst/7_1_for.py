3255c657b1c668f15e5e06272d5d341029fdd1ca
@vla78dan <vla78dan@gmail.com>
2026-08-31 11:42:11 +0300

    pyForDjango_GenPyFirst_7_1_for18

--- pyForDjango/generationFirst/7_1_for.py
+++ pyForDjango/generationFirst/7_1_for.py
@@ -33,10 +33,76 @@ s1 = input()
 n = int(input())
 for i in range(n):
     print(s1)
-"""
+
 n = int(input())
 for i in range(n):
     print(19 * "*")
+
+
+for i in range(4):
+    print(i, end='*')
+
+
+for i in range(4):
+    j = i + 1
+    print(i, j)
+
+
+for i in range(27):
+    j = i + 1
+    print(i)
+
+print(j)
+
+
+total = 0
+for i in range(3):
+    total = total - i
+    print(total)
+
+
+
+total = 0
+for i in range(10):
+    total = total + i
+    if i == 9:
+        total = 1
+
+print(total)
+
+
+str1 = input()
+
+num = -1
+for i in range(10):
+    num = num + 1
+    print(num, str1)
+
+
+text = input()
+for i in range(10):
+    print(i, text)
+"""
+
+
+num = int(input()) + 1
+
+for i in range(num):
+    print('Квадрат числа ' + str(i) + ' равен ' + str((i * i)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
