7ba4589166b041ee6070549271c6e52036d2b316
@vla78dan <vla78dan@gmail.com>
2026-09-08 18:17:11 +0300

    BestOfPyTh_8_end

--- BestInPyThird/8_for/8_1_Go.py
+++ BestInPyThird/8_for/8_1_Go.py
@@ -34,8 +34,74 @@ for i in range(num1):
     print(str1)
 
 
-"""
 
+str1 = input()
+str2 = input()
+str3 = input()
+num = int(input())
+for i in range(num):
+    print(str1, str2, str3, sep='\n')
+
+
+
+bal = 1000
+for i in range(5):
+    oper = input()
+    summ_oper = int(input())
+    if oper == 'списание':
+        bal -= summ_oper
+    elif oper == 'зачисление':
+        bal += summ_oper
+print(f'Ваш итоговый баланс: {bal}')
+
+
+bal = 1000
+for i in range(5):
+    oper = input()
+    summ_oper = int(input())
+    if oper == 'зачисление':
+        bal += summ_oper
+    if oper == 'списание' and (summ_oper < bal):
+        bal -= summ_oper
+    else:
+        print('Списание невозможно, недостаточно средств')
+
+print(f'Ваш итоговый баланс: {bal}')
+
+bal = 1000
+
+for i in range(5):
+    oper = input()
+    summ_oper = int(input())
+    if oper == 'зачисление':
+        bal += summ_oper
+    elif oper == 'списание':
+        if summ_oper <= bal:
+            bal -= summ_oper
+        else:
+            print('Списание невозможно, недостаточно средств')
+print(f'Ваш итоговый баланс: {bal}')
+
+
+bal = 0
+
+n_income = int(input())
+n_outcome = int(input())
+for i in range(n_income):
+    bashli_income = int(input())
+    bal += bashli_income
+for i in range(n_outcome):
+    bashli_outcome = int(input())
+    bal -= bashli_outcome
+print(f'Ваш итоговый баланс: {bal}')
+
+"""
+verno = 0
+for i in range(6):
+    str1 = input()
+    if str1 == "искусство":
+        verno += 1
+print(f"Правильно введённых слов: {verno}")
 
 
 
