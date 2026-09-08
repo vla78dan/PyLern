7cfa7f39fa2d8623beda57e14d02e6bd23289d86
@vla78dan <vla78dan@gmail.com>
2026-09-08 13:53:45 +0300

    BestOfPyTh_8_1

---
+++ BestInPyThird/8_for/8_1_Go.py
@@ -0,0 +1,41 @@
+"""
+
+
+
+total_n = 0
+
+for i in range(4):
+    money = int(input())
+    total_n += money
+print(total_n)
+
+
+total_spend = 0
+for i in range(5):
+    spend = int(input())
+    total_spend += spend
+print(total_spend)
+
+
+
+
+count_spent = int(input())
+total_spend = 0
+
+for i in range(count_spent):
+    spend = int(input())
+    total_spend += spend
+print(total_spend)
+
+str1 = input()
+num1 = int(input())
+
+for i in range(num1):
+    print(str1)
+
+
+"""
+
+
+
+
